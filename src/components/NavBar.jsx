519edec24e76be5136c1fdb3fbde3f74526f1b0d
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-05-07 20:36:18 +0300

    done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import './NavBar/NavBar.css'
+import React from "react";
+import "./NavBar/NavBar.css";
 
 const NavBar = () => {
   return (
-    <div className='navbar'>
-        <ul className="navbar-menu">
-            <li>Home</li>
-            <li>menu</li>
-            <li>mobile-app</li>
-            <li>contact us</li>
-        </ul>
-        <div className='navbar-search-icon'> 
-        </div>
+    <div className="navbar">
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navbar-search-icon"></div>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
