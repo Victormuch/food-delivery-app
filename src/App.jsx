2d7857a4983f0430ab45742a08973fcd0b992ca3
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-05-07 12:13:13 +0300

    added image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 function App() {
-  return(
-   <div>
-    <h1>Hello World</h1>
-    <p>This is a food App</p>
-  </div>
-  )
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a food App</p>
+      <img src={require("./images/home.jpg")} alt="" />
+    </div>
+  );
 }
 
 export default App;
