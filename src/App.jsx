cb2b480302fe6ee00c2b3c665c5c22165c29aae2
Abraham Kinuthia <abrahamkinuthia193@gmail.com>
2024-05-07 22:14:24 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
 
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+       <Header />
+      <Footer />
     </div>
   );
 }
