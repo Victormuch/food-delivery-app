e09fb1c651df4f52ac7e8bd446373c608c0dd486
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-05-07 21:55:23 +0300

    blablabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <Footer/>
     </div>
   );
 }
