3c64853e16d942596afb7a59312ab665df0806a3
iqramahamed <iqra.mahamed64@gmail.com>
2024-05-07 22:09:03 +0300

    app.jsx done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
 
+
 function App() {
   return (
     <div className="app">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,17 @@
-import React, { useState } from "react";
-import "./NavBar/NavBar.css";
-
+import React, { useState } from 'react'
+import "./NavBar/NavBar.css"
 
 const NavBar = () => {
-  const [menu, setMenu] = useState("home");
+
+    const [menu, setMenu] = useState("home")
 
   return (
-    <div className="navbar">
-      <ul className="navbar-menu">
-        <li
-          onClick={() => setMenu("home")}
-          className={menu === "home" ? "active" : ""}
-        >
-          Home
-        </li>
-        <li
-          onClick={() => setMenu("menu")}
-          className={menu === "menu" ? "active" : ""}
-        >
-          Menu
-        </li>
-        <li
-          onClick={() => setMenu("contact-us")}
-          className={menu === "contact-us" ? "active" : ""}
-        >
-          Contact Us
-        </li>
-      </ul>
+    <div className='navbar'>
+        <ul className="navbar-menu">
+            <li onClick={() => setMenu("home")}  className={menu=== "home"?"active":""}>Home</li>
+            <li onClick={() => setMenu("menu")} className={menu=== "menu"?"active" :""}>Menu</li>
+            <li onClick={() => setMenu("contact-us")} className={menu=== "contact-us"?"active" :""}>Contact Us</li>
+        </ul>
     </div>
   );
 };
