f3cab8a52fc8555ce0b7041f8adfe5acfc2cb89c
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-05-07 16:47:41 +0300

    server1

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,3 @@ function App() {
 }
 
 export default App;
-// Fettucine pasta
-// Deborah Ndole
-// 3:42 PM
-// biryani
-// Iqra Mahamed
-// 3:42 PM
-// Butter Chicken
-// Deborah Ndole
-// 3:42 PM
-// pilau
-// Iqra Mahamed
-// 3:42 PM
-// Pad Thai
-// Chicken fajitas
-// Abraham Kinuthia
-// 3:44 PM
-// Aki iqra you're written food stuffs I've never heard before
-// Iqra Mahamed
-// 3:44 PM
-// Pancakes
-// Sushi
-// Iqra Mahamed
-// 3:46 PM
-// Chicken sandwich
-// Abraham Kinuthia
-// 3:47 PM
-// Greek spanakopita
-// Iqra Mahamed
-// 3:47 PM
-// Noodle Stir Fry with Vegetables.
-// Deborah Ndole
-// 3:47 PM
-// shawarma
