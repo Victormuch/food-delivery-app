2f2806881b1ac13b5c54b4ac351a143b28e558e4
Abrahamkinuthia4723 <162336259+Abrahamkinuthia4723@users.noreply.github.com>
2024-05-07 22:23:42 +0300

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
 
             </div>
             <div className="right-side">
-                <h2>Welcome to Foodies</h2>
+                <h2>Welcome to Foodies!</h2>
                 <p>There is no love sincerer than the love of food.</p>
                 <p>Foodies restraurant thereby invites you all to come and experience this love</p>
                 <p>Although a great restaurant experience must include great food, a bad restaurant experience can be achieved through bad service alone. Ideally, service invaluable.That is why Our committed team of delivery experts guarantees that your orders are delivered promptly and attentively, ensuring that your meal arrives hot and fresh on every occasion.</p>  
