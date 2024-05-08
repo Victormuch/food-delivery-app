f76c4c0507fdb06be305724e47412593831acacf
iqramahamed <iqra.mahamed64@gmail.com>
2024-05-08 10:36:51 +0300

    no conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
-
-
-
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
-
 import About from "./components/About/About";
 import Menu from "./components/Menu/Menu";
+import PlaceOrder from "./components/PlaceOrder/PlaceOrder"; // Assuming you have a component for PlaceOrder
 
-
-
-
-function App() => {
+function App() {
   return (
-    <div className="app">
-      <NavBar />
-      <Routes>
-
-        < Routes path= '/' element= {<Home/>} />
-      </Routes path= '/cart' element={<cart/>}/>
-      < Routes path ='/order' elements={<PlaceOrder/>}>
-
-      </Routes>
-
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-       <Header />
-       <About/>
-
-       <Menu/>
-
-       <AppDownload/>
-
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+        <Header />
+        <About />
+        <Menu />
+        { <AppDownload />}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
