f5eddc6de834b89abc50390a983e7772dd8431e2
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-05-08 14:29:03 +0300

    donneee

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
 import "./NavBar/NavBar.css";
+import {Link} from "react-router-dom"
+
 
 const NavBar = () => {
 
     const [menu, setMenu] = useState("home")
     return (
-    
-    <div className="navbar">
-      <ul className="navbar-menu">
-        <li
-          onClick={() => setMenu("home")}
-          className={menu === "home" ? "active" : ""}
-        >
-          Home
-        </li>
-        <li
-          onClick={() => setMenu("menu")}
-          className={menu === "menu" ? "active" : ""}
-        >
-          Menu
-        </li>
-        <li
-          onClick={() => setMenu("contact-us")}
-          className={menu === "contact-us" ? "active" : ""}
-        >
-          Contact Us
-        </li>
-      </ul>
-     </div>
-
-  )
+      <div className="navbar">
+        <ul className="navbar-menu">
+          <li
+            onClick={() => setMenu("home")}
+            className={menu === "home" ? "active" : ""}
+          >
+            <Link to="/">Home</Link>
+          </li>
+          <li
+            onClick={() => setMenu("menu")}
+            className={menu === "menu" ? "active" : ""}
+          >
+            <Link to="/menu">Menu</Link>
+          </li>
+          <li
+            onClick={() => setMenu("contact-us")}
+            className={menu === "contact-us" ? "active" : ""}
+          >
+            <Link to="/contact-us">Contact Us</Link>
+          </li>
+        </ul>
+      </div>
+    );
 }
 
 
